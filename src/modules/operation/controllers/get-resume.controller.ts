9810c77b4389f2060127e0caf1842a387b6d973c
João Brasil <jvobsystems@gmail.com>
2024-12-06 20:49:40 -0300

    feat: adicionando a documentação openApi

--- src/modules/operation/controllers/get-resume.controller.ts
+++ src/modules/operation/controllers/get-resume.controller.ts
@@ -4,7 +4,16 @@ import { AbstractGetResumeUseCase } from '../use-cases/abstract-get-resume.use-c
 import { Express } from 'express';
 import { plainToClass } from 'class-transformer';
 import { GetResumeResponseDto } from '@src/shared/dtos/get-resume-response.dto';
-import { ApiResponseProperty, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { IResume } from '@src/shared/interfaces/get-resume.interface';
+
+class Resume {
+  @ApiProperty()
+  amount: number;
+
+  @ApiProperty()
+  resume: Object;
+}
 
 @Controller('resumes')
 @ApiTags('Operation')
@@ -16,8 +25,10 @@ export class GetResumeController {
   @Post()
   @HttpCode(HttpStatus.OK)
   @UseInterceptors(FileInterceptor('file'))
-  @ApiResponseProperty({
-    type: GetResumeResponseDto
+  @ApiOkResponse({
+    description: 'Response resume',
+    type: Resume,
+
 })
   async execute(@UploadedFile() file: Express.Multer.File): Promise<GetResumeResponseDto> {
     const buffer = file.buffer;
